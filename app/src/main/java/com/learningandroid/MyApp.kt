2a2b5fcd05f3028026a8a74102037b40f71170bc
tateno <tateno-masa@gmail.com>
2023-05-04 14:04:12 +0900

    Timber導入

--- app/src/main/java/com/learningandroid/MyApp.kt
+++ app/src/main/java/com/learningandroid/MyApp.kt
@@ -1,7 +1,22 @@
 package com.learningandroid
 
 import android.app.Application
+import com.learningandroid.log.AppTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MyApp : Application()
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupLogger()
+    }
+
+    private fun setupLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(AppTree())
+        }
+    }
+}
